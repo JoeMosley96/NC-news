5931e0970419c40e661756ef0b527b558165db4d
Joe Mosley <joemosley96@gmail.com>
2024-05-29 11:29:54 +0100

    removes stray console.logs

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,10 +1,8 @@
 const {fetchArticle} = require("../models/articles.models")
 
 const getArticle = (req, res, next) =>{
-    console.log(req.params, "<--req")
     fetchArticle(req.params.article_id)
     .then((data)=>{
-        console.log(data)
         res.status(200).send({article:data})
     })
     .catch((err)=>{
