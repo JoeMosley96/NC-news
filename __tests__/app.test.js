c84d237b13aa2963231a46e2c7823a3ca05f881c
Joe Mosley <joemosley96@gmail.com>
2024-05-29 10:17:12 +0100

    simplifies the collection of data from the index file in the app.test.js file- i.e no destructuring in the test file testing changed so that, if the topics folder is empty, we receive an empty array not a 404 error

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,78 +1,93 @@
-const {topicData, articleData, userData, commentData} = require("../db/data/test-data/index")
-const request = require ("supertest")
-const app = require("../app")
-const connection = require("../db/connection")
-const seed = require("../db/seeds/seed")
-const endpoints = require("../endpoints.json")
+const data = require("../db/data/test-data/index");
+const request = require("supertest");
+const app = require("../app");
+const connection = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const endpoints = require("../endpoints.json");
 
-
-beforeEach(()=>{
-return seed({topicData, articleData, userData, commentData})
-})
-
-afterAll(()=>{connection.end()})
-
-describe('GET /api/topics', () => {
-    it('200: should respond with a 200 status code ', () => {
-        return request(app).get("/api/topics").expect(200)
-    });
-
-    it("200: should return an array", () => {
-        return request(app)
-          .get("/api/topics")
-          .then((data) => {
-            expect(Array.isArray(data.body)).toBe(true);
-          });
-      });
-
-      it("200 should respond with an array of all topics", () => {
-        return request(app)
-          .get("/api/topics")
-          .expect(200)
-          .then(({ body }) => {
-            const topics = body;
-    
-            expect(topics).toHaveLength(3);
-
-            topics.forEach((topic) => {
-              expect(topic).toMatchObject({
-                description: expect.any(String),
-                slug: expect.any(String),
-              });
-            });
-          });
-      });
-
-      it("404 should respond with a 404 error code if passed an invalid route", () =>{
-        return request(app)
-        .get("/notARoute")
-        .expect(404)
-        .then((data)=>{
-            expect(data.body.msg).toBe("Route not found")
-        })
-      })
+beforeEach(() => {
+  return seed(data);
 });
 
-describe('GET /api', () => {
-    it('200: should respond with a 200 status code', () => {
-        return request(app)
-        .get("/api")
-        .expect(200)        
-    });
-    it('200: should return the endpoints json file', () => {
-        return request(app)
-        .get("/api")
-        .then((data)=>{
-            expect(data.body.endpoints).toEqual(endpoints)
-        })        
-    });
-    it('200: should return the endpoints json file', () => {
-        return request(app)
-        .get("/api")
-        .then((data)=>{
-            expect(data.body.endpoints).toEqual(endpoints)
-        })        
-    });
+afterAll(() => {
+  connection.end();
+});
 
-    
+describe("GET /api/topics", () => {
+  it("200: should respond with a 200 status code ", () => {
+    return request(app).get("/api/topics").expect(200);
+  });
+
+  it("200: should return an array", () => {
+    return request(app)
+      .get("/api/topics")
+      .then((data) => {
+        expect(Array.isArray(data.body)).toBe(true);
+      });
+  });
+
+  it("200 should respond with an array of all topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const topics = body;
+
+        expect(topics).toHaveLength(3);
+
+        topics.forEach((topic) => {
+          expect(topic).toMatchObject({
+            description: expect.any(String),
+            slug: expect.any(String),
+          });
+        });
+      });
+  });
+
+  it("200 should respond with an empty array if the topics folder is empty", () => {
+    return connection
+      .query("DELETE FROM comments")
+      .then(() => {
+        return connection.query("DELETE FROM articles");
+      })
+      .then(() => {
+        return connection.query("DELETE FROM topics");
+      })
+      .then(() => {
+        return request(app).get("/api/topics").expect(200);
+      })
+      .then((data) => {
+        expect(data.body).toEqual([]);
+      });
+  });
+
+  it("404 should respond with a 404 error code if passed an invalid route", () => {
+    return request(app)
+      .get("/notARoute")
+      .expect(404)
+      .then((data) => {
+        expect(data.body.msg).toBe("Route not found");
+      });
+  });
+
+});
+
+describe("GET /api", () => {
+  it("200: should respond with a 200 status code", () => {
+    return request(app).get("/api").expect(200);
+  });
+  it("200: should return the endpoints json file", () => {
+    return request(app)
+      .get("/api")
+      .then((data) => {
+        expect(data.body.endpoints).toEqual(endpoints);
+      });
+  });
+  it("200: should return the endpoints json file", () => {
+    return request(app)
+      .get("/api")
+      .then((data) => {
+        expect(data.body.endpoints).toEqual(endpoints);
+      });
+  });
 });
